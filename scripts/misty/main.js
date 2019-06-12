dada0a1cbfa02e8a40c2c2c92e6053e4943460c3
Misty DeRamus <mistyderamus@gmail.com>
2019-06-12 11:52:00 -0500

    add to DOM

--- scripts/misty/main.js
+++ scripts/misty/main.js
@@ -1,1 +1,13 @@
-const eventbrite
+// add concert search component to the DOM
+
+function createConcertSearchComponent(concertObj) {
+    return `
+    <h2>${concertObj.text}</h2>
+    <h2>${}</h2>
+    
+`
+  }
+  function AddConcertComponentToDom(concertComponent, element){
+  document.querySelector(`#${}`).innerHTML += concerntComponent
+}
+// taking an object to create the HTML representation to place it in the DOM

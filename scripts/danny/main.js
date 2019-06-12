d2603e4ee0239edbcc94064fe26b5a82ea835d95
Danny Barker <barkerdanny17@yahoo.com>
2019-06-11 20:59:47 -0500

    connected API

--- scripts/danny/main.js
+++ scripts/danny/main.js
@@ -1,1 +1,14 @@
-const eventbrite
+const zomBtn = document.querySelector("#zomatoBtn");
+zomBtn.addEventListener('click', () => {
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=${zomato.user_key}&start=first&sort=rating`)
+      
+    .then( data => data.json())
+    .then( restaurantList => {
+        restaurantList.restaurants.forEach( eatery => {
+            console.log('restaurantList :', eatery.restaurant.R.res_id);
+            
+        });
+        
+    })
+    
+})

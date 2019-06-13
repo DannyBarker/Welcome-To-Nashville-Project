c57826ed299b1129a63ffef70702b607d74d96dc
Danny Barker <barkerdanny17@yahoo.com>
2019-06-13 13:11:19 -0500

    got restaurant button to save

--- scripts/danny/main.js
+++ scripts/danny/main.js
@@ -8,23 +8,39 @@ zomBtn.addEventListener('click', () => {
         let wordReturn = restaurantList.restaurants;
         wordReturn.forEach( keyWord => {
             createRestaurantOption(keyWord.restaurant)
-        });
         if (document.querySelector("#test").innerHTML === "<h1>Results:</h1>") {
             document.querySelector("#test").innerHTML = 
             `<h1>Results:</h1>
             <p>Unable to find "${searchText.value}":(</p>
                 `
-            }
+        }
+
         })
+        let savRestaurant = document.querySelector("#test")
+        for (let i = 0; i < savRestaurant.childNodes.length; i++) {
+            let one = savRestaurant.childNodes[i].childNodes
+            if (savRestaurant.childNodes[i].childNodes[9] !== undefined) {
+                savRestaurant.childNodes[i].childNodes[9].addEventListener('click', () => {
+                    itineraryArr.restaurant.name = `${one[1].innerText}`
+                    itineraryArr.restaurant.url = `${one[1].childNodes[3].innerHTML}`
+                    makeItinerary(itineraryArr)
+                })
+            }
+            };   
+           
         searchText.value = "";
-        
     })
+})
+
     
     const createRestaurantOption = (newObj) => {
         document.querySelector("#test").innerHTML += 
         `
         <div id="createRestaurant">
-            <a href="${newObj.url}" target="_blank" class="noDecoration"><h3 class="header">${newObj.name}</h3></a>
+            <div>
+                <a href="${newObj.url}" target="_blank" class="noDecoration"><h3 class="header">${newObj.name}</h3></a>
+                <p hidden>${newObj.url}</p>
+             </div>
             <h5>Rating:</h5>
             <p class="rating">${newObj.user_rating.aggregate_rating}</p>
             <address>Address: ${newObj.location.address}<br>
@@ -34,7 +50,11 @@ zomBtn.addEventListener('click', () => {
             <button id="save">Save</button>
         </div>
         `
-        
     }
-    
-    
+const makeItinerary = (arr) => {
+   document.querySelector(".content").innerHTML =
+    `
+        <h3>Restaurant:</h3>
+        <a href="${arr.restaurant.url}" target="_blank"><p>${arr.restaurant.name}</p></a>
+    `
+}
---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+{/* <div id="intineraryHTML">
+            <section id="itineraryDOM">
+                <div class="content">
+                <h2>Itinerary:</h1>
+                <h4>Park:</h4>
+                <h4>Restaurant:</h4>
+                <h4>Meetup:</h4>
+                <h4>Concert</h4>
+            </div>
+            </section>
+    </div> */}
+
+let itineraryArr = {
+    park: "",
+    restaurant: {
+        name: "",
+        url: "",
+    },
+    meetup: "",
+    concert: "",
+}

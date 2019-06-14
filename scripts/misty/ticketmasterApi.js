ed9faf4afdf8052946735b786588f60a0f62c608
Misty DeRamus <mistyderamus@gmail.com>
2019-06-14 08:10:01 -0500

    deleted old comments

--- scripts/misty/ticketmasterApi.js
+++ scripts/misty/ticketmasterApi.js
@@ -1,4 +1,3 @@
-// taking an object to create the HTML representation to place it in the DOM
 const concertId = document.querySelector("#button-is-primary");
 const inputId = document.querySelector("#ticketmasterInput");
 document.querySelector("#button-is-primary").addEventListener("click", () => {
@@ -12,11 +11,6 @@ document.querySelector("#button-is-primary").addEventListener("click", () => {
     .then(data => data.json())
     .then(concertList => {
       document.querySelector("#test").innerHTML = "";
-      //   console.log("concert list", concertList);
-      //   console.log(concertList._embedded.events);
-      //   for (let i = 0; i < concertList._embedded.events.length; i++) {
-      //     console.log(concertList._embedded.events[i].classifications);
-      //add to the dOM
       for (let i = 0; i < concertList._embedded.events.length; i++) {
         const ticketmasterInput = concertList._embedded.events[i];
         createConcertSearchResults(ticketmasterInput);
